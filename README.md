b1cd5a1fa972124abf93659cb1d8ae37afb34532
daknhh <david@globaldatanet.com>
2021-06-17 11:11:25 +0200

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# stacksetinstance-cleanup
+## General info
+This Tool is to cleanup all StackSet Instances for one AWS Account.
+
+![Example](static/example-output.png)
+
+## Tool info:
+|Version |Author  | 
+--- | --- |
+|1.0 | David Krohn </br> [Linkedin](https://www.linkedin.com/in/daknhh/) - [Blog](https://globaldatanet.com/blog/author/david-krohn)|
+
+
+## Usage
+- `python cleanup-stacksets.py --account ACCOUNTID --retain yes/no --profile PROFILE`
+
+### INFO 
+
+- ACCOUNTID
+    Define target accountid
+- RETAIN
+    Choose if you want to retain the stack or not (yes / no)
+- PROFILE
+    Define AWS Profile - should be the AWS Account where the StackSets are managed
---
+++ cleanup-stacksets.py
@@ -0,0 +1,66 @@
+import boto3
+import datetime
+import argparse
+import time
+from tqdm import tqdm
+
+def delete_stack_set_instances(accountid,client,retain):
+    allStack_sets = client.list_stack_sets(
+        Status='ACTIVE',
+        CallAs='SELF'
+    )
+    if retain == 'yes':
+        RetainStacks = True
+    else:
+        RetainStacks = False
+
+
+    for stackset in tqdm(allStack_sets['Summaries'],desc="Progress"):
+        instances = client.list_stack_instances(
+        StackSetName=stackset['StackSetName'],
+        StackInstanceAccount=accountid,
+        CallAs='SELF')
+        
+        if instances['Summaries'] != []:
+            print(f"\n\nDeleting StackSetInstances {stackset['StackSetName']}")
+            for instance in instances['Summaries']:
+                print(f"🌍 {instance['Region']}")
+                response = client.delete_stack_instances(
+                StackSetName=stackset['StackSetName'],
+                Accounts=[
+                    accountid,
+                ],
+                Regions=[
+                    instance['Region'],
+                ],
+                RetainStacks=RetainStacks,
+                CallAs='SELF'
+                )
+                operationid = response['OperationId']
+                finished = False
+                while not finished:
+                    time.sleep(5)
+                    status = client.describe_stack_set_operation(StackSetName=stackset['StackSetName'], OperationId=operationid)["StackSetOperation"]["Status"]
+                    if status in 'RUNNING':
+                        finished = False
+                    else:
+                        finished = True
+                if status not in 'SUCCEEDED':
+                    print(f"💀 StackSet Operation failed with Status: {status}")         
+                print(f"✅ - All StackSet Instances for {stackset['StackSetName']} has been deleted.") 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--account', help='--account define target accountid')
+parser.add_argument('--profile', help='--profile enter valid AWSPROFILE')
+parser.add_argument('--retain', help='--retain choose if you want to retain the stack or not yes / no')
+args = parser.parse_args()
+session = boto3.Session(profile_name=args.profile)
+cloudformation_client = session.client('cloudformation')
+print(f'\n\n💣 Delete StackSet Instances for one AWS Account 💀\n')
+print(f'👨🏻‍💻 - linkedin.com/in/daknhh 🔀 daknhh\n\n ')
+print(f"""⚙️  SETTINGS: \n Profile: {args.profile} \n Account: {args.account} \n Retain: {args.retain} \n""")
+delete_stack_set_instances(args.account,cloudformation_client, args.retain)
+
+generationTime = datetime.datetime.now()
+print(f"""\n🗓: {generationTime}""")
+
